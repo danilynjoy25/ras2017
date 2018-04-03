a496717b962be41c0a5f6b1274e87b56f685d70a
Katrina <katrina.trinidad@obf.ateneo.edu>
2018-04-04 02:38:25 +0800

    Modified mail.php

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'katrina.trinidad@obf.ateneo.edu'),
+        'name' => env('MAIL_FROM_NAME', 'RAS2017'),
     ],
 
     /*
